788e3bd195be39647fc43c0c04f268401a4f90cb
HenryNgu1 <110834511+HenryNgu1@users.noreply.github.com>
2025-02-06 23:59:49 +1100

    1.2P
    Completed

---
+++ 1.2P - Object Orientated Hello World/1.2  - Object Orientated Programming/1.2  - Object Orientated Programming/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2____Object_Orientated_Programming
+{
+    public class Message
+    {
+        private string _text;
+        public Message(string text) 
+        {
+            _text = text;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_text);
+        }
+    }
+}
